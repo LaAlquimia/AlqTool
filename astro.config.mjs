b0f8e9b06b3b004002aa57a22a359fa46044a4d2
La Alquimia <jccanchila@unal.edu.co>
2024-02-21 09:37:36 -0500

    HOist

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://laalquimia.github.io',
-	base: '/alqtool',
+	base: '/AlqTool',
 	integrations: [mdx(), sitemap()],
 });
